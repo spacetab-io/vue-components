3e815ed4edb58a7d78652147f141df9fa2f0d3f6
Михалев Эдуард Викторович <eduard.mikhalev@sbermegamarket.ru>
2023-10-02 19:08:50 +0300

    Published branch "1.0.0"

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+    <h1>Spacetab UI</h1>
+     <st-icon name="calendar"></st-icon>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'app',
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
-declare module 'vue/types/vue' {
-  interface Vue {
-    $ELEMENT?: {
-      size: string,
-    }
-  }
-}
+import { createApp } from "vue"
+import App from './App.vue';
+import SpacetabUI from './index';
+// declare module 'vue/types/vue' {
+//   interface Vue {
+//     $ELEMENT?: {
+//       size: string,
+//     }
+//   }
+// }
+
+
+const app = createApp(App);
+app.use(SpacetabUI);
+app.mount('#app');
